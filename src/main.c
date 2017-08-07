85dc45233bf1d23548e39dc9d062af02d800b9b9
Jesus Flores <joy_warmgun@hotmail.com>
2017-08-07 18:07:57 -0500

    Agregando alguna seguridad al paso de parametros  On branch master 	modified:   Makefile 	modified:   main.c

--- src/main.c
+++ src/main.c
@@ -1,6 +1,22 @@
 #include <stdio.h>
 
+int options(int argc, char *argv[]);
+
 int main(int argc, char *argv[])
 {
-    printf("Hola mundo\n");
+    int ret;
+
+    ret = options(argc, argv);
+    if (ret != 0)
+	return 1;
+
+    printf("Hola mundo %s\n", argv[1]);
+}
+
+int options(int argc, char *argv[])
+{
+    if (argc != 2)
+	return 1;
+
+    return 0;
 }
