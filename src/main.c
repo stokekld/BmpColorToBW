9c005d16f4635414ee066d6db3a654ad0fe412ee
Jesus Flores <joy_warmgun@hotmail.com>
2017-08-07 20:11:14 -0500

    Obteniendo los octetos del archivo  On branch master 	modified:   src/main.c

--- src/main.c
+++ src/main.c
@@ -5,12 +5,31 @@ int options(int argc, char *argv[]);
 int main(int argc, char *argv[])
 {
     int ret;
+    int c;
 
     ret = options(argc, argv);
     if (ret != 0)
 	return 1;
 
-    printf("Hola mundo %s\n", argv[1]);
+    FILE *img = fopen(argv[1], "rb");
+
+    if (img == NULL)
+    {
+	perror("No se pudo abrir el archivo.");
+	return 1;
+    }
+
+    do{
+	c = fgetc(img);
+
+	if (c != EOF)
+	    printf("%x\n", c);
+
+    }while(c != EOF);
+
+
+    fclose(img);
+
 }
 
 int options(int argc, char *argv[])
